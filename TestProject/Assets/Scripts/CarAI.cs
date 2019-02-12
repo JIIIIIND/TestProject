10ae546f574140dae73a64db4bb320d0c47ae965
JIND-LAPTOP <jinwoo700@gmail.com>
2019-02-12 13:53:28 +0900

    바닥면 RAY무시하도록 실험

--- TestProject/Assets/Scripts/CarAI.cs
+++ TestProject/Assets/Scripts/CarAI.cs
@@ -14,25 +14,37 @@ public class CarAI : MonoBehaviour {
     private RaycastHit raycastHit;
 
     [SerializeField]
-    private List<GameObject> rayPosition;
+    private GameObject rayPosition;
 
 	void Start () {
 		
 	}
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (Physics.BoxCast(rayPosition.transform.position, transform.lossyScale / 2, transform.forward, out raycastHit, Quaternion.identity, rayRange))
+        {
+            Gizmos.DrawRay(transform.position, transform.forward * raycastHit.distance);
+            Gizmos.DrawWireCube(transform.position + transform.forward * raycastHit.distance, transform.lossyScale);
+        }
+        else
+            Gizmos.DrawRay(transform.position, transform.forward * rayRange);
+    }
     private void ShootRay()
     {
-        foreach(GameObject rayPos in rayPosition)
-        if (Physics.Raycast(rayPos.transform.position, rayPos.transform.TransformDirection(Vector3.forward), out raycastHit, rayRange))
+        
+        //if (Physics.Raycast(rayPosition.transform.position, rayPosition.transform.TransformDirection(Vector3.forward), out raycastHit, rayRange))
+        if (Physics.BoxCast(rayPosition.transform.position, transform.lossyScale/2, transform.forward, out raycastHit, Quaternion.identity, rayRange))
         {
-            Debug.DrawRay(rayPos.transform.position, rayPos.transform.TransformDirection(Vector3.forward) * raycastHit.distance, Color.yellow);
+            Debug.DrawRay(rayPosition.transform.position, transform.forward * raycastHit.distance, Color.yellow);
             Debug.Log(raycastHit.collider + " :" + raycastHit.distance);
             RotationBody(raycastHit, speedValue);
             //SpeedDown(speedValue);
         }
         else
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * rayRange, Color.blue);
+            Debug.DrawRay(transform.position, transform.forward * rayRange, Color.blue);
             SpeedUP(speedValue);
         }
     }
@@ -41,7 +53,7 @@ public class CarAI : MonoBehaviour {
     {
         Vector3 reflect = Vector3.Reflect(transform.TransformDirection(Vector3.forward), hit.normal);
         Debug.DrawRay(hit.transform.position, reflect, Color.red);
-
+        SpeedUP(value / 2);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(reflect), rotationValue * Time.deltaTime);
         //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.FromToRotation(transform.TransformDirection(Vector3.forward), reflect), rotationValue);
     }

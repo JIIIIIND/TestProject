60de0b6fc5c5244ef856e049db1ac7625cc75e65
JIND <jinwoo700@gmail.com>
2019-04-06 15:57:31 +0900

    VRTK에셋 추가, 스테이지 변경 로직 작성

--- TestProject/Assets/Scripts/UIController.cs
+++ TestProject/Assets/Scripts/UIController.cs
@@ -9,8 +9,11 @@ public class UIController : MonoBehaviour {
     [SerializeField] private GameObject StoryUI;
     [SerializeField] private GameObject TreatmentUI;
 
+    [SerializeField] private GameObject[] stages;
+    private int currentStage;
+
     void Start () {
-		
+        currentStage = 0;
 	}
 	
     public void PushBackButton(GameObject gameObject)
@@ -36,6 +39,42 @@ public class UIController : MonoBehaviour {
     {
         gameObject.SetActive(false);
     }
+
+    public void NextStage()
+    {
+        if (currentStage < stages.Length)
+        {
+            currentStage++;
+        }
+        else
+        {
+            Debug.Log("this is last stage");
+        }
+        UpdateStageImage();
+    }
+
+    public void PreviousStage()
+    {
+        if(currentStage > 0)
+        {
+            currentStage--;
+        }
+        else
+        {
+            Debug.Log("this is first stage");
+        }
+        UpdateStageImage();
+    }
+
+    private void UpdateStageImage()
+    {
+        float imageSize = stages[currentStage].GetComponent<UnityEngine.UI.Image>().rectTransform.rect.width;
+        Vector3 parentTransform = stages[currentStage].GetComponentInParent<RectTransform>().localPosition;
+
+        parentTransform.Set(parentTransform.x - imageSize * currentStage, parentTransform.y, parentTransform.z);
+        stages[currentStage].GetComponentInParent<RectTransform>().localPosition = parentTransform;
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
